3bd0108a6c16d0703b840a2ee4246238339bde5d
Matiasr09YT <Matiasr09YT@users.noreply.github.com>
2021-12-03 17:43:07 -0500

    fix

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -340,7 +340,7 @@ class StoryMenuState extends MusicBeatState
 			PlayState.SONG = Song.loadFromJson(poop, PlayState.storyPlaylist[0]);
 			PlayState.storyWeek = curWeek;
 			PlayState.campaignScore = 0;
-			destroyFreeplayVocals();
+			FreeplayState.destroyFreeplayVocals();
 		/*	new FlxTimer().start(1, function(tmr:FlxTimer)
 			{
 				LoadingState.loadAndSwitchState(new PlayState(), true);
